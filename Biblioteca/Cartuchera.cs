007b69cf5e79430494686ed8c20f7bf184e08209
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-11-17 22:14:16 -0300

    definiendo clase Util

---
+++ Biblioteca/Cartuchera.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Cartuchera<T> where T : Util
+    {
+
+        private int capacidad;
+        private List<T> listaElementos;
+
+        public Cartuchera(int capacidad)
+        {
+            this.capacidad = capacidad;
+            listaElementos = new List<T>();
+        }
+
+        public decimal PrecioTotal
+        {
+            get { return 0; }
+        }
+
+    }
+}
--- Biblioteca/Util.cs
+++ Biblioteca/Util.cs
@@ -2,7 +2,16 @@
 
 namespace Biblioteca
 {
-    public class Util
+    public abstract class Util
     {
+        private int id;
+        private decimal precio;
+
+        public int Id { get { return id; } }
+        public decimal Precio 
+        { 
+            get { return precio; } 
+            set { precio = value; }
+        }
     }
 }
