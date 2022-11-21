66d32dcb67aef339a7d51c3b839cb72a9b402beb
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-11-21 14:01:15 -0300

    avanzo con los elementos de FrmModificar()

--- UI/FrmModificarElemento.Designer.cs
+++ UI/FrmModificarElemento.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.gpb_Controles = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gpb_Controles.SuspendLayout();
             this.SuspendLayout();
             // 
             // gpb_Controles
@@ -36,12 +40,42 @@
             this.gpb_Controles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.gpb_Controles.Controls.Add(this.label3);
+            this.gpb_Controles.Controls.Add(this.label2);
+            this.gpb_Controles.Controls.Add(this.label1);
             this.gpb_Controles.Location = new System.Drawing.Point(12, 12);
             this.gpb_Controles.Name = "gpb_Controles";
             this.gpb_Controles.Size = new System.Drawing.Size(291, 352);
             this.gpb_Controles.TabIndex = 0;
             this.gpb_Controles.TabStop = false;
             this.gpb_Controles.Text = " ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
             // 
             // FrmModificarElemento
             // 
@@ -52,6 +86,8 @@
             this.Name = "FrmModificarElemento";
             this.Text = "FrmModificarElemento";
             this.Load += new System.EventHandler(this.FrmModificarElemento_Load);
+            this.gpb_Controles.ResumeLayout(false);
+            this.gpb_Controles.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +95,8 @@
         #endregion
 
         private System.Windows.Forms.GroupBox gpb_Controles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
